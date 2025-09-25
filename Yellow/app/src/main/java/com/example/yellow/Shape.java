6003d6c82e3abc4a7a46cad57c3ff3b05f9ace00
WaylonWang17 <waylon.wang17@gmail.com>
2025-09-24 18:43:01 -0600

    main push

--- Yellow/app/src/main/java/com/example/yellow/Shape.java
+++ Yellow/app/src/main/java/com/example/yellow/Shape.java
@@ -1,7 +1,8 @@
 package com.example.yellow;
 
-public abstract class Shape {
+public abstract class Shape{
     // As a team
     private int x_field;
     private int y_field;
+    String color = "green";
 }
